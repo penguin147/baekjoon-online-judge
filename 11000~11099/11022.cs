b8fecbc18a46cab17921c92eba3c197fabf5d108
junseop <jay000111@naver.com>
2023-05-01 21:41:39 +0900

    2023/05/01

---
+++ 11000~11099/11022.cs
@@ -0,0 +1,27 @@
+﻿/*
+문제
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+출력
+각 테스트 케이스마다 "Case #x: A + B = C" 형식으로 출력한다.
+x는 테스트 케이스 번호이고 1부터 시작하며, C는 A+B이다.
+*/
+
+
+int N, A, B;
+string[] input;
+
+N = Convert.ToInt32(Console.ReadLine());
+input = new string[N];
+
+for(int i = 0; i < N; i++)
+{
+    input = Console.ReadLine().Split(' ');
+    A = Convert.ToInt32(input[0]);
+    B = Convert.ToInt32(input[1]);
+    Console.WriteLine("Case #" + (i+1) + ": " + A + " + " + B + " = " + (A + B));
+}
